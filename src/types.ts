3693e5bb55e192fa370d0ba274cc52a487898dc3
Michael Szmadzinski <smujmaiku@gmail.com>
2025-01-26 18:44:56 -0500

    deno fixed eventemitter3 typing

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,3 @@
-import { TypedEmitter } from 'tiny-typed-emitter';
-
-// eventemitter3 doesn't cooperate with Deno so well
-declare module 'peerjs' {
-	type ErrorType = string;
-	type RTCIceConnectionState = unknown;
-
-	interface Peer extends
-		TypedEmitter<{
-			open: (id: string) => void;
-			connection: (dataConnection: DataConnection) => void;
-			call: (mediaConnection: MediaConnection) => void;
-			close: () => void;
-			disconnected: (currentId: string) => void;
-			error: (error: PeerError<`${PeerErrorType}`>) => void;
-		}> {}
-	interface DataConnection extends
-		TypedEmitter<{
-			data: (data: unknown) => void;
-			open: () => void;
-			close: () => void;
-			error: (error: PeerError<`${ErrorType}`>) => void;
-			iceStateChanged: (state: RTCIceConnectionState) => void;
-		}> {}
-}
-
 // immer likes to extends from this
 export type AnyObject = {
 	[key: string]: any;
